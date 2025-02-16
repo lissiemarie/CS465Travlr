04856c1e3e6cc207878e842e4993452f9ce9778b
lissiemarie <elyssamunden@gmail.com>
2025-02-16 17:27:38 -0600

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -19,6 +19,54 @@ const tripsList = async(req, res) => {
     }
 };
 
+//POST: /trips - Adds a new Trip
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+
+    const q = await newTrip.save();
+
+    if(!q) {
+        return res.status(404).json(err);
+    } else {
+        return res.status(201).json(q);
+    }
+};
+
+//PUT: /trips/:tripCode - Adds a new trip
+const tripsUpdateTrip = async(req, res) => {
+    const q = await Model
+        .findOneAndUpdate(
+            {'code': req.params.tripCode},
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+
+        if(!q) {
+            return res.status(400).json(err);
+        } else {
+            return res.status(201).json(q)
+        }
+        console.log(q);
+}
+
 const tripsFindByCode = async(req, res) => {
     const q = await Model
         .find({'code' : req.params.tripCode})
@@ -37,5 +85,7 @@ const tripsFindByCode = async(req, res) => {
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
